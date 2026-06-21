334ef1f6934e25e68be9772e7174c6f92b0ee0e2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:01:01 +0400

    add fields with lombok annotation

--- demo/src/main/java/com/fooddelivery/demo/Entities/CustomerAddress.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/CustomerAddress.java
@@ -1,4 +1,17 @@
 package com.fooddelivery.demo.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerAddress {
+    private String street;
+    private String city;
+    private String building;
+    private Boolean isDefault;
+    private Customer customer;
+
 }
