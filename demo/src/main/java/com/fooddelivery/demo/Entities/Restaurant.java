d6f194c1b18c1e5db126c50375532181064589d2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:26:22 +0400

    add Restaurant  class extends BaseEntity with fields

--- demo/src/main/java/com/fooddelivery/demo/Entities/Restaurant.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Restaurant.java
@@ -1,6 +1,34 @@
 package com.fooddelivery.demo.Entities;
 
-public class Restaurant extends BaseEntity{
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant extends BaseEntity {
+    private String name;
+    private String description;
+    private String cuisineType;
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+    private BigDecimal minOrderAmount;
+    private BigDecimal deliveryFee;
+    private Boolean acceptingOrders;
+    @ManyToOne
+    private RestaurantOwner restaurantOwner;
+    @OneToMany()
+    private List<MenuItem> menuItems;
+    @OneToMany()
+    private List<ComboMeal> comboMeals;
 
 
 }
