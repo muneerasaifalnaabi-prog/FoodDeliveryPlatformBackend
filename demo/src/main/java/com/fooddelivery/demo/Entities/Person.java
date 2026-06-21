f149c5e5346786b0dcb15f7e5f47f2989e3643aa
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 19:15:41 +0400

    add Person class with fields with lombok anotation

--- demo/src/main/java/com/fooddelivery/demo/Entities/Person.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Person.java
@@ -1,5 +1,21 @@
 package com.fooddelivery.demo.Entities;
 
-public class Person extends  BaseEntity{
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String phone;
+    private String passwordHash;
 
 }
