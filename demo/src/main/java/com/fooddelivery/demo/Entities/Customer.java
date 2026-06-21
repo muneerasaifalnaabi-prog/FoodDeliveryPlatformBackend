f9d9226b75fbe44cbc47a5c78d45a179e518d7c8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 19:17:08 +0400

    update customer class use Persont that extends from it

--- demo/src/main/java/com/fooddelivery/demo/Entities/Customer.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Customer.java
@@ -1,14 +1,12 @@
 package com.fooddelivery.demo.Entities;
 
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Data
@@ -16,19 +14,13 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 public class Customer extends Person {
-    private String firstName;
-    private String lastName;
-    @Column(unique = true)
-    private String email;
-    private String phone;
-    private String passwordHash;
     private Integer loyaltyPoints;
     private String customerCode;
     @OneToMany()
     private List<CustomerAddress> addresses;
     @OneToMany()
-    private List<Order> orders ;
+    private List<Order> orders;
     @OneToMany()
-    private List<Review> reviews ;
+    private List<Review> reviews;
 
 }
