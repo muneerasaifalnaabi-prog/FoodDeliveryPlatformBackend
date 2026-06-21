65e6597f4eed4c47e69f2229f5e832a1a33ff242
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:32:48 +0400

    add MenuItem   class extends BaseEntity with fields

--- demo/src/main/java/com/fooddelivery/demo/Entities/MenuItem.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/MenuItem.java
@@ -1,4 +1,21 @@
 package com.fooddelivery.demo.Entities;
 
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class MenuItem extends BaseEntity{
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Boolean isAvailable;
+    private Boolean isVegetarian;
+    private Integer calories;
+    @ManyToOne
+    private Restaurant restaurant;
+    @OneToMany()
+    private List<OrderItem> orderItems;
+
 }
--- demo/src/main/java/com/fooddelivery/demo/Entities/Restaurant.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Restaurant.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
+import java.util.List;
 
 @Entity
 @Data
