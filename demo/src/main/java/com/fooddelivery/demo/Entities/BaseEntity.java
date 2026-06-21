1df9fb1f2a84437ccc8b716fe0d6a0c619806225
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 17:40:53 +0400

    add fields with lombok annotation

--- demo/src/main/java/com/fooddelivery/demo/Entities/BaseEntity.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/BaseEntity.java
@@ -1,4 +1,25 @@
 package com.fooddelivery.demo.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private Boolean isActive = true;
+
 }
