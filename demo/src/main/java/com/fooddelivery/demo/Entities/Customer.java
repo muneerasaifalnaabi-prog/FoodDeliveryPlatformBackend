a1e3ebf10e5218a731a31e5c161b01df2cdc2dd3
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 17:48:33 +0400

    add fields with lombok annotation

--- demo/src/main/java/com/fooddelivery/demo/Entities/Customer.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Customer.java
@@ -1,8 +1,21 @@
 package com.fooddelivery.demo.Entities;
 
-public class Customer extends BaseEntity{
+
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+public class Customer extends BaseEntity {
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
+    private String phone;
+    private String passwordHash;
+    private Integer loyaltyPoints;
+    private String customerCode;
+    @OneToMany()
+    private List<CustomerAddress> addresses;
 
 }
