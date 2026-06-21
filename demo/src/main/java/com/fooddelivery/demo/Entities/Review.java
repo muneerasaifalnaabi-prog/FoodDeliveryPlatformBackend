8224d80ace53737eec9a689666857ccd0fbaabc4
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:51:51 +0400

    add Review class extends BaseEntity with fields

--- demo/src/main/java/com/fooddelivery/demo/Entities/Review.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Review.java
@@ -1,4 +1,27 @@
 package com.fooddelivery.demo.Entities;
 
-public class Review {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    private String targetType;
+    private Integer rating;
+    private String comment;
+    private LocalDateTime createdAt;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private Restaurant restaurant;
+    @ManyToOne
+    private DeliveryDriver deliveryDriver;
+
 }
