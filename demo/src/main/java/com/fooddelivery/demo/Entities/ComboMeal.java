53edd8d4865e6285bf5da98cddd31979d4544139
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:35:47 +0400

    add ComboMeal class extends BaseEntity with fields

--- demo/src/main/java/com/fooddelivery/demo/Entities/ComboMeal.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/ComboMeal.java
@@ -1,11 +1,19 @@
 package com.fooddelivery.demo.Entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class ComboMeal extends BaseEntity {
     private String comboName;
     private String description;
