7d3a4cb7055e7b0f889983cf75e4453144810c72
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:21:24 +0400

    add RestaurantOwner  class extends BaseEntity with fields

--- demo/src/main/java/com/fooddelivery/demo/Entities/RestaurantOwner.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/RestaurantOwner.java
@@ -1,4 +1,24 @@
 package com.fooddelivery.demo.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RestaurantOwner extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String passwordHash;
+    private String businessLicenseCode;
+    @OneToMany()
+    private List<Restaurant> restaurants;
 }
