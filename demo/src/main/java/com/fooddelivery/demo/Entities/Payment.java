8b281d9528bb532f38b1af201d9dbd5dca650905
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:49:05 +0400

    add Payment class extends BaseEntity with fields

--- demo/src/main/java/com/fooddelivery/demo/Entities/Payment.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Payment.java
@@ -1,4 +1,25 @@
 package com.fooddelivery.demo.Entities;
 
-public class Payment {
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment extends BaseEntity {
+    private String paymentMethod;
+    private String status;
+    private BigDecimal amount;
+    private String transactionRef;
+    private LocalDateTime processedAt;
+    @OneToOne
+    private Order order;
+
 }
