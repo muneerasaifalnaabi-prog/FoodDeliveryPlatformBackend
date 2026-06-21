3cee20a2701db8b8583b0c1f5245e0cd4cc375be
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-21 18:17:50 +0400

    add Delivery  class extends BaseEntity

--- demo/src/main/java/com/fooddelivery/demo/Entities/Delivery.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/Delivery.java
@@ -1,10 +1,18 @@
 package com.fooddelivery.demo.Entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Delivery extends BaseEntity {
     private String trackingCode;
     private String status;
