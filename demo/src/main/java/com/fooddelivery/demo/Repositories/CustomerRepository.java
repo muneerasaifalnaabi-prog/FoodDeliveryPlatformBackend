ebb4cf3c9b305267d0123817646bca0e98857f66
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 09:40:25 +0400

    add findCustomersRegisteredBetween  method with @Query

--- demo/src/main/java/com/fooddelivery/demo/Repositories/CustomerRepository.java
+++ demo/src/main/java/com/fooddelivery/demo/Repositories/CustomerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
@@ -14,6 +15,10 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     @Query("SELECT c FROM Customer c WHERE c.loyaltyPoints >= :points AND c.isActive = true")
     List<Customer> findByLoyaltyPointsGreaterThanEqual(@Param("points") int points);
+
+    @Query(" SELECT c FROM Customer c WHERE c.createdDate BETWEEN :startDate AND :endDate AND c.isActive = true")
+    List<Customer> findCustomersRegisteredBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
 
 
 
