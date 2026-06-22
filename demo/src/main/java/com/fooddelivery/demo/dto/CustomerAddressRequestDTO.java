e018f25b6f235cebd315bfd7bc2c48940f735643
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 14:28:12 +0400

    add fields and convertToDTO method with  jakarta.validation

--- demo/src/main/java/com/fooddelivery/demo/dto/CustomerAddressRequestDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/CustomerAddressRequestDTO.java
@@ -1,4 +1,34 @@
 package com.fooddelivery.demo.dto;
 
+import com.fooddelivery.demo.Entities.CustomerAddress;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class CustomerAddressRequestDTO {
+    @NotBlank(message = "Street is required")
+    private String street;
+
+    @NotBlank(message = "City is required")
+    private String city;
+
+    @NotBlank(message = "Building is required")
+    private String building;
+
+    private Boolean isDefault;
+
+    public CustomerAddress convertToDTO() {
+
+        CustomerAddress address = new CustomerAddress();
+
+        address.setStreet(street);
+        address.setCity(city);
+        address.setBuilding(building);
+        address.setIsDefault(isDefault != null ? isDefault : false);
+
+        return address;
+    }
+
 }
