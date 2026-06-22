adf8c38a2c550bc8fd7c910ac53f2f07b8c2d1d9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 14:18:40 +0400

    add convertToDTO method

--- demo/src/main/java/com/fooddelivery/demo/dto/CustomerResponseDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/CustomerResponseDTO.java
@@ -1,10 +1,12 @@
 package com.fooddelivery.demo.dto;
 
+import com.fooddelivery.demo.Entities.Customer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerResponseDTO {
     private Integer id;
     private String firstName;
@@ -13,5 +15,20 @@ public class CustomerResponseDTO {
     private String phone;
     private Integer loyaltyPoints;
     private String customerCode;
+
+    public static CustomerResponseDTO convertToDTO(Customer entity) {
+
+        CustomerResponseDTO dto = new CustomerResponseDTO();
+
+        dto.setId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setEmail(entity.getEmail());
+        dto.setPhone(entity.getPhone());
+        dto.setLoyaltyPoints(entity.getLoyaltyPoints());
+        dto.setCustomerCode(entity.getCustomerCode());
+
+        return dto;
+    }
 
 }
