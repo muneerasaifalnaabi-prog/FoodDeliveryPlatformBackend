878e763df18cc1b014a536ca0913d04ce1c49c69
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 14:52:57 +0400

    add fields and convertToDTO method with  jakarta.validation

--- demo/src/main/java/com/fooddelivery/demo/dto/RestaurantOwnerResponseDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/RestaurantOwnerResponseDTO.java
@@ -1,4 +1,46 @@
 package com.fooddelivery.demo.dto;
 
+import com.fooddelivery.demo.Entities.RestaurantOwner;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class RestaurantOwnerResponseDTO {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String businessLicenseCode;
+
+
+    public static RestaurantOwnerResponseDTO convertToDTO(RestaurantOwner entity) {
+
+        RestaurantOwnerResponseDTO dto = new RestaurantOwnerResponseDTO();
+
+        dto.setId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setEmail(entity.getEmail());
+        dto.setPhone(entity.getPhone());
+        dto.setBusinessLicenseCode(entity.getBusinessLicenseCode());
+
+        return dto;
+    }
+
+    public static List<RestaurantOwnerResponseDTO> convertToDTO(List<RestaurantOwner> entities) {
+
+        List<RestaurantOwnerResponseDTO> dtos = new ArrayList<>();
+
+        for (RestaurantOwner entity : entities) {
+            dtos.add(convertToDTO(entity));
+        }
+
+        return dtos;
+    }
+
 }
