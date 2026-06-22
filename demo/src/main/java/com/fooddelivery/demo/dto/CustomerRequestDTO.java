954f47fe6bf5aea28bb32d14453f8dff21535a6f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 14:11:18 +0400

    add fields and convertToDTO method with  jakarta.validation

--- demo/src/main/java/com/fooddelivery/demo/dto/CustomerRequestDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/CustomerRequestDTO.java
@@ -1,5 +1,9 @@
 package com.fooddelivery.demo.dto;
 
+import com.fooddelivery.demo.Entities.Customer;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +12,36 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerRequestDTO {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please enter a valid email address")
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+?[0-9]{8,15}$",
+            message = "Phone number must contain only numbers and be between 8 and 15 digits")
+    private String phone;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public Customer convertToDTO() {
+
+        Customer customer = new Customer();
+
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setEmail(email);
+        customer.setPhone(phone);
+        customer.setPasswordHash(password);
+        customer.setLoyaltyPoints(0);
+
+        return customer;
+    }
 
 }
