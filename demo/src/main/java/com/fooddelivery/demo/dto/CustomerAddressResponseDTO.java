9b75836933907cbf6eabc54f84cb028c7db2b270
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 14:36:37 +0400

    add fields and convertToDTO method with  jakarta.validation

--- demo/src/main/java/com/fooddelivery/demo/dto/CustomerAddressResponseDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/CustomerAddressResponseDTO.java
@@ -1,4 +1,42 @@
 package com.fooddelivery.demo.dto;
 
+import com.fooddelivery.demo.Entities.CustomerAddress;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class CustomerAddressResponseDTO {
+    private Integer id;
+    private String street;
+    private String city;
+    private String building;
+    private Boolean isDefault;
+
+    public static CustomerAddressResponseDTO convertToDTO(CustomerAddress entity) {
+
+        CustomerAddressResponseDTO dto = new CustomerAddressResponseDTO();
+
+        dto.setId(entity.getId());
+        dto.setStreet(entity.getStreet());
+        dto.setCity(entity.getCity());
+        dto.setBuilding(entity.getBuilding());
+        dto.setIsDefault(entity.getIsDefault());
+
+        return dto;
+    }
+
+    public static List<CustomerAddressResponseDTO> convertToDTO(List<CustomerAddress> entities) {
+
+        List<CustomerAddressResponseDTO> dtos = new ArrayList<>();
+
+        for (CustomerAddress entity : entities) {
+            dtos.add(convertToDTO(entity));
+        }
+
+        return dtos;
+    }
 }
