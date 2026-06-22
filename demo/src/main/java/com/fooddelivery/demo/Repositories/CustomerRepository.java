89602773a65c9151a1700eada9c7aa6169c13e40
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 09:36:21 +0400

    add findByEmail method with @Query

--- demo/src/main/java/com/fooddelivery/demo/Repositories/CustomerRepository.java
+++ demo/src/main/java/com/fooddelivery/demo/Repositories/CustomerRepository.java
@@ -1,7 +1,17 @@
 package com.fooddelivery.demo.Repositories;
 
 import com.fooddelivery.demo.Entities.Customer;
+import org.antlr.v4.runtime.atn.SemanticContext;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Query("SELECT c FROM Customer c WHERE c.email = :email AND c.isActive = true")
+    List<Customer> findByEmail(@Param("email") String email);
+
+
+
 }
