4ab268915a7ce09823f4f8e9eb0364392a49da47
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-22 14:17:17 +0400

    add needed fields

--- demo/src/main/java/com/fooddelivery/demo/dto/CustomerResponseDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/CustomerResponseDTO.java
@@ -1,4 +1,17 @@
 package com.fooddelivery.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CustomerResponseDTO {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Integer loyaltyPoints;
+    private String customerCode;
+
 }
