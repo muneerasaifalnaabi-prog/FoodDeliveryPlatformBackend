5e55fb39c257dc44cfcad82504001b1dae7f4732
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-23 11:45:47 +0400

    add PaymentSummaryDTO class with fromEntity method

--- demo/src/main/java/com/fooddelivery/demo/dto/SummaryDTO/PaymentSummaryDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/SummaryDTO/PaymentSummaryDTO.java
@@ -1,4 +1,26 @@
 package com.fooddelivery.demo.dto.SummaryDTO;
 
+import com.fooddelivery.demo.Entities.Payment;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
 public class PaymentSummaryDTO {
+    private Integer Id;
+    private String paymentMethod;
+    private String status;
+    private BigDecimal amount;
+
+    public static PaymentSummaryDTO fromEntity(Payment payment) {
+        PaymentSummaryDTO dto = new PaymentSummaryDTO();
+        dto.setId(payment.getId());
+        dto.setPaymentMethod(payment.getPaymentMethod());
+        dto.setStatus(payment.getStatus());
+        dto.setAmount(payment.getAmount());
+
+        return dto;
+    }
 }
