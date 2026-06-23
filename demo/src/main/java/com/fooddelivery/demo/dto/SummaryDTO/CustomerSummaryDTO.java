d640cb4867503b882e141a5d8a9bd519ec3e69f2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-23 09:38:25 +0400

    add CustomerSummaryDTO

--- demo/src/main/java/com/fooddelivery/demo/dto/SummaryDTO/CustomerSummaryDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/SummaryDTO/CustomerSummaryDTO.java
@@ -1,4 +1,22 @@
 package com.fooddelivery.demo.dto.SummaryDTO;
 
+import com.fooddelivery.demo.Entities.Customer;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class CustomerSummaryDTO {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public static CustomerSummaryDTO fromEntity(Customer customer) {
+        CustomerSummaryDTO dto = new CustomerSummaryDTO();
+        dto.setId(customer.getId());
+        dto.setFirstName(customer.getFirstName());
+        dto.setLastName(customer.getLastName());
+        return dto;
+    }
+
 }
