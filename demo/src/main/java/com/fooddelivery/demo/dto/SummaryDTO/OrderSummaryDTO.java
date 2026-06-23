51c4bdd625d59f5d40d8df8731c569317d68ee35
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-23 11:50:00 +0400

    add OrderSummaryDTO class with fromEntity method

--- demo/src/main/java/com/fooddelivery/demo/dto/SummaryDTO/OrderSummaryDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/SummaryDTO/OrderSummaryDTO.java
@@ -1,4 +1,26 @@
 package com.fooddelivery.demo.dto.SummaryDTO;
 
+import com.fooddelivery.demo.Entities.Orders;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
 public class OrderSummaryDTO {
+    private Integer Id;
+    private String orderCode;
+    private String status;
+    private BigDecimal totalAmount;
+
+    public static OrderSummaryDTO fromEntity(Orders orders){
+        OrderSummaryDTO dto = new OrderSummaryDTO();
+        dto.setId(orders.getId());
+        dto.setOrderCode(orders.getOrderCode());
+        dto.setStatus(orders.getStatus());
+        dto.setTotalAmount(orders.getTotalAmount());
+
+        return dto;
+    }
 }
