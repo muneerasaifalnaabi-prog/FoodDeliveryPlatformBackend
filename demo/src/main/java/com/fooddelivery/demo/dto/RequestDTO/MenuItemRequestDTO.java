6791cdf05f345ad9909e163fda25024eb3131f90
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-23 08:15:04 +0400

    add ComboMealRequestDTO  class

--- demo/src/main/java/com/fooddelivery/demo/dto/RequestDTO/MenuItemRequestDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/RequestDTO/MenuItemRequestDTO.java
@@ -1,5 +1,39 @@
 package com.fooddelivery.demo.dto.RequestDTO;
 
+import com.fooddelivery.demo.Entities.MenuItem;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class MenuItemRequestDTO {
+    @NotBlank(message = "Item name is required")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero")
+    private Double price;
+
+    @NotNull(message = "Vegetarian flag must be specified")
+    private Boolean isVegetarian;
+
+    @PositiveOrZero(message = "Calories cannot be negative")
+    private Integer calories;
+
+    public MenuItemRequestDTO convertToDTO(MenuItem entity) {
+        MenuItemRequestDTO item = new MenuItemRequestDTO();
+        item.setName(entity.getName());
+        item.setDescription(entity.getDescription());
+        item.setPrice(entity);
+        item.setIsVegetarian(this.isVegetarian);
+        item.setCalories(this.calories);
+        item.setIsAvailable(true);
+        return item;
 
 }
