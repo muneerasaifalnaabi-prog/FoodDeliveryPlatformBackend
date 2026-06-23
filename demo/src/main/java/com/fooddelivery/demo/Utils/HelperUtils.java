fd40aa249ff37a74a310ee5f1798ec12b41e48f2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-23 16:28:03 +0400

    add HelperUtils with generateCode

--- demo/src/main/java/com/fooddelivery/demo/Utils/HelperUtils.java
+++ demo/src/main/java/com/fooddelivery/demo/Utils/HelperUtils.java
@@ -1,4 +1,9 @@
 package com.fooddelivery.demo.Utils;
 
 public class HelperUtils {
+
+    public static String generateCode(String prefix) {
+        int randomNumber = (int) (Math.random() * 9000) + 1000;
+        return prefix + "-" + randomNumber;
+    }
 }
