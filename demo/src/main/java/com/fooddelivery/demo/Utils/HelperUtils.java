1bf2e79c28337a16d8c7f9efa2219ab89ff77393
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-23 16:29:29 +0400

    add HelperUtils with overloaded generateCode

--- demo/src/main/java/com/fooddelivery/demo/Utils/HelperUtils.java
+++ demo/src/main/java/com/fooddelivery/demo/Utils/HelperUtils.java
@@ -6,4 +6,13 @@ public class HelperUtils {
         int randomNumber = (int) (Math.random() * 9000) + 1000;
         return prefix + "-" + randomNumber;
     }
+    public static String generateCode(String prefix, int length) {
+
+        StringBuilder code = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int digit = (int) (Math.random() * 10);
+            code.append(digit);
+        }
+        return prefix + "-" + code;
+    }
 }
