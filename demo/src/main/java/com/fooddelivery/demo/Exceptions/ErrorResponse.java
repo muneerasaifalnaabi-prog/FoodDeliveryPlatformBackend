c3015ca826c65a14ae24b0a5b9fca49a83ffd468
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-23 14:00:27 +0400

    add ErrorResponse with fields

---
+++ demo/src/main/java/com/fooddelivery/demo/Exceptions/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.fooddelivery.demo.Exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private Date timestamp;
+    private int statusCode;
+    private HttpStatus status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse(HttpStatus status, int statusCode, String error, String message, String path) {
+        this.status = status;
+        this.statusCode = statusCode;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
