0d25a94bbf7c7d6135fa700ad81a85cb6de0ef54
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-24 09:26:28 +0400

    update DuplicateResourceException

--- demo/src/main/java/com/fooddelivery/demo/Exceptions/DuplicateResourceException.java
+++ demo/src/main/java/com/fooddelivery/demo/Exceptions/DuplicateResourceException.java
@@ -1,7 +1,26 @@
 package com.fooddelivery.demo.Exceptions;
 
+import org.springframework.http.HttpStatus;
+
 public class DuplicateResourceException extends RuntimeException {
+    private final HttpStatus status;
+
     public DuplicateResourceException(String message) {
         super(message);
+        this.status = HttpStatus.CONFLICT;
+    }
+
+    public DuplicateResourceException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public static DuplicateResourceException alreadyExists(String resourceName, String value) {
+
+        return new DuplicateResourceException(HttpStatus.CONFLICT, resourceName + " '" + value + "' already exists.");
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
