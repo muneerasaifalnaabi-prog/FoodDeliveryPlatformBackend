cd78890e71897b67d1c1ff504e991efaed5d9675
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-25 00:10:31 +0400

    add updateDriverLocation  method

--- demo/src/main/java/com/fooddelivery/demo/Services/DeliveryService.java
+++ demo/src/main/java/com/fooddelivery/demo/Services/DeliveryService.java
@@ -8,6 +8,7 @@ import com.fooddelivery.demo.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.demo.Repositories.DeliveryDriverRepository;
 import com.fooddelivery.demo.Repositories.DeliveryRepository;
 import com.fooddelivery.demo.Repositories.OrdersRepository;
+import com.fooddelivery.demo.dto.ResponseDTO.DeliveryDriverResponseDTO;
 import com.fooddelivery.demo.dto.ResponseDTO.DeliveryResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -43,9 +44,10 @@ public class DeliveryService {
         delivery.setIsActive(true);
         return DeliveryResponseDTO.fromEntity(deliveryRepository.save(delivery));
     }
-    public  DeliveryResponseDTO autoAssignDriver( Integer orderId ) {
-        Orders orders=ordersRepository.findById(orderId).orElseThrow(() -> ResourceNotFoundException.notFound("Order", orderId));
-        DeliveryDriver driver =deliveryDriverRepository.findFirstAvailableOnlineDriver().orElseThrow(() -> ResourceNotFoundException.notFound("Driver", orderId));
+
+    public DeliveryResponseDTO autoAssignDriver(Integer orderId) {
+        Orders orders = ordersRepository.findById(orderId).orElseThrow(() -> ResourceNotFoundException.notFound("Order", orderId));
+        DeliveryDriver driver = deliveryDriverRepository.findFirstAvailableOnlineDriver().orElseThrow(() -> ResourceNotFoundException.notFound("Driver", orderId));
 
         if (!orders.getStatus().equals("CONFIRMED")) {
             throw InvalidOrderStateException.invalidState("cannot assign driver to order");
@@ -62,5 +64,16 @@ public class DeliveryService {
 
         return DeliveryResponseDTO.fromEntity(deliveryRepository.save(delivery));
     }
+
+    public DeliveryDriverResponseDTO updateDriverLocation(Integer driverId, double lat, double lng) {
+        DeliveryDriver driver = deliveryDriverRepository.findDriverById(driverId).orElseThrow(() -> ResourceNotFoundException.notFound("Driver", driverId));
+
+        driver.setCurrentLat(lat);
+        driver.setCurrentLng(lng);
+        driver.setUpdatedDate(LocalDateTime.now());
+
+        return DeliveryDriverResponseDTO.fromEntity(deliveryDriverRepository.save(driver));
+    }
+
 }
 
