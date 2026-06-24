681691062af47c51f85ae06ae13b12d60a84eb4f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-24 09:22:16 +0400

    update InvalidOrderStateException

--- demo/src/main/java/com/fooddelivery/demo/Exceptions/InvalidOrderStateException.java
+++ demo/src/main/java/com/fooddelivery/demo/Exceptions/InvalidOrderStateException.java
@@ -1,7 +1,24 @@
 package com.fooddelivery.demo.Exceptions;
 
+import org.springframework.http.HttpStatus;
+
 public class InvalidOrderStateException extends RuntimeException {
+    private final HttpStatus status;
+
     public InvalidOrderStateException(String message) {
         super(message);
+        this.status = HttpStatus.CONFLICT;
+    }
+
+    public InvalidOrderStateException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+    public static InvalidOrderStateException conflict(String message) {
+
+        return new InvalidOrderStateException(HttpStatus.CONFLICT, message);
+    }
+    public HttpStatus getStatus() {
+        return status;
     }
 }
--- demo/src/main/java/com/fooddelivery/demo/Services/CustomerService.java
+++ demo/src/main/java/com/fooddelivery/demo/Services/CustomerService.java
@@ -1,4 +1,37 @@
 package com.fooddelivery.demo.Services;
 
+
+import com.fooddelivery.demo.Exceptions.ResourceNotFoundException;
+import com.fooddelivery.demo.Repositories.CustomerAddressRepository;
+import com.fooddelivery.demo.Repositories.CustomerRepository;
+import com.fooddelivery.demo.dto.RequestDTO.CustomerRequestDTO;
+import com.fooddelivery.demo.dto.ResponseDTO.CustomerResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+    private CustomerRepository customerRepository;
+       @Autowired
+    public CustomerService(CustomerAddressRepository customerAddressRepository) {
+        this.customerAddressRepository = customerAddressRepository;
+    }
+
+    private CustomerAddressRepository customerAddressRepository;
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public CustomerResponseDTO  createCustomer(CustomerRequestDTO dto){
+        if (dto==null){
+            throw new ResourceNotFoundException("");
+        }
+    }
+
+
+
+
+
 }
