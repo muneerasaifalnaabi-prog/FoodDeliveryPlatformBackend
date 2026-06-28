a39756a20b654910b62396917b44cc54c6464036
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-28 14:47:12 +0400

    Add fields with toEntity method

--- demo/src/main/java/com/fooddelivery/demo/dto/RequestDTO/RestaurantLocationRequestDTO.java
+++ demo/src/main/java/com/fooddelivery/demo/dto/RequestDTO/RestaurantLocationRequestDTO.java
@@ -1,4 +1,20 @@
 package com.fooddelivery.demo.dto.RequestDTO;
 
+import com.fooddelivery.demo.Entities.RestaurantLocation;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RestaurantLocationRequestDTO {
+    private Double latitude;
+    private Double longitude;
+
+    public RestaurantLocation toEntity() {
+        RestaurantLocation restaurantLocation = new RestaurantLocation();
+        restaurantLocation.setLatitude(latitude);
+        restaurantLocation.setLongitude(longitude);
+        return restaurantLocation;
+
+    }
 }
