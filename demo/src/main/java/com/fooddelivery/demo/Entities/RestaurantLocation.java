3f2ffc6d4de5fc6d05c0c4bf39add5bc2bb22867
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-28 11:13:11 +0400

    added fields

--- demo/src/main/java/com/fooddelivery/demo/Entities/RestaurantLocation.java
+++ demo/src/main/java/com/fooddelivery/demo/Entities/RestaurantLocation.java
@@ -1,4 +1,18 @@
 package com.fooddelivery.demo.Entities;
 
-public class RestaurantLocation {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class RestaurantLocation extends BaseEntity{
+    private Double latitude;
+    private Double longitude;
+    @ManyToOne
+    private Restaurant restaurant;
 }
